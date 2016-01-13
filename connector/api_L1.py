f3013b3724de1dc8c7c21c8bc157fccb2d63119c
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-01-12 22:29:33 -0600

    Initial commit of redo of API, currently working

---
+++ connector/api_L1.py
@@ -0,0 +1,339 @@
+# Copyright 2014-2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+import requests as r
+import json
+from base64 import standard_b64decode as b64decode
+import threading
+import sys
+import traceback
+from connectorError import response_codes
+
+class asyncResult:
+	"""For use as part of connector library calls that make asycnronous calls.
+		For simple GET calls wait untill the is_done flag is set then read the result and check the status_code.
+		For complex calls like Notifications the next_step variable will move the asynch-callback to a notification """
+
+	def isDone(self):
+		return self.is_done
+
+	def fill(self,  data):
+		if type(data) == r.models.Response:
+			try:
+				self.result =  json.loads(data.content)
+			except:
+				self.result = []
+				if isinstance(data.content,str): # string handler
+					self.result = data.content
+				elif isinstance(data.content,int): # int handler
+					print "data returned is an integer, not sure what to do with that"
+				else: # all other handler
+					print "unhandled data type, type of content : %s" %type(data.content)
+			self.status_code = data.status_code
+			self.raw_data = data.content
+		else:
+			#error
+			print("type not found : %s"%type(data))
+		return
+
+	def __init__(self, callback=""):
+		self.is_done = False
+		self.result = {}
+		self.status_code = ''
+		self.raw_data = {}
+		self.callback = callback
+		self.next_step = ""
+		self.extra = {}
+		self.error = ""
+		self.endpoint = ""
+		self.resource = ""
+
+class connector:
+	"""Class to create connector objects and manage connections"""
+
+	# Return connector version number and recent rest API version number it supports
+	def getConnectorVersion(self):
+		result = asyncResult()
+		data = self._getURL("/",versioned=False)
+		result.fill(data)
+		if data.status_code == 200:
+			result.error = False
+		else:
+			result.error = response_codes("get_mdc_version",data.status_code)
+		result.is_done = True
+		return result
+
+	# Return API version of connector
+	def getApiVersion(self):
+		result = asyncResult()
+		data = self._getURL("/rest-versions",versioned=False)
+		result.fill(data)
+		if data.status_code == 200:
+			result.error = False
+		else:
+			result.error = response_codes("get_rest_version",data.status_code)
+		result.is_done = True
+		return result
+
+	# Returns metadata about connector limits as JSON blob
+	def getLimits(self):
+		result = asyncResult()
+		data = self._getURL("/limits")
+		result.fill(data)
+		if data.status_code == 200:
+			result.error = False
+		else:
+			result.error = response_codes("limit",data.status_code)
+		result.is_done = True
+		return result
+
+	# return json list of all endpoints.
+	# optional type field can be used to match all endpoints of a certain type.
+	def getEndpoints(self,typeOfEndpoint=""):
+		q = {}
+		result = asyncResult()
+		if typeOfEndpoint:
+			q['type'] = typeOfEndpoint
+			result.extra['type'] = typeOfEndpoint
+		data = self._getURL("/endpoints", query = q)
+		result.fill(data)
+		if data.status_code == 200:
+			result.error = False
+		else:
+			result.error = response_codes("get_endpoints",data.status_code)
+		result.is_done = True
+		return result
+
+	# return json list of all resources on an endpoint
+	def getResources(self,ep,noResp=False,cacheOnly=False):
+		# load query params if set to other than defaults
+		q = {}
+		result = asyncResult()
+		result.endpoint = ep
+		if noResp or cacheOnly:
+			q['noResp'] = 'true' if noResp == True else 'false'
+			q['cacheOnly'] = 'true' if cacheOnly == True else 'false'
+		# make query
+		data = self._getURL("/endpoints/"+ep, query=q)
+		result.fill(data)
+		# check sucess of call
+		if data.status_code == 200: # sucess
+			result.error = False
+		else: # fail
+			result.error = response_codes("get_resources",data.status_code)
+		result.is_done = True
+		return result
+
+
+	# return async object
+	def getResourceValue(self,ep,res,cbfn="",noResp=False,cacheOnly=False):
+		q = {}
+		result = asyncResult(callback=cbfn) #set callback fn for use in async handler
+		result.endpoint = ep
+		result.resource = res
+		if noResp or cacheOnly:
+			q['noResp'] = 'true' if noResp == True else 'false'
+			q['cacheOnly'] = 'true' if cacheOnly == True else 'false'
+		# make query
+		data = self._getURL("/endpoints/"+ep+res, query=q)
+		result.fill(data)
+		if data.status_code == 200: # immediate success
+			result.error = False
+			result.is_done = True
+			return cbfn(result)
+		elif data.status_code == 202:
+			self.database['async-responses'][json.loads(data.content)["async-response-id"]]= result
+		else: # fail
+			result.error = response_codes("get_resources",data.status_code)
+			result.is_done = True
+		return result
+
+	# return async object
+	def putResourceValue(self,ep,res,data):
+		result = asyncResult()
+		result.endpoint = ep
+		result.resource = res
+		data = self._putURL("/endpoints/"+ep+res,json=data, versioned=False)
+		if data.status_code == 200: #immediate success
+			result.error = False
+			result.isDone = True
+		elif data.status_code == 202:
+			self.database['async-responses'][json.loads(data.content)["async-response-id"]]= result
+		else:
+			result.error = response_codes("get_resources",data.status_code)
+			result.is_done = True
+		result.raw_data = data.content
+		result.status_code = data.status_code
+		return result
+
+
+
+
+	# this function needs to spin off a thread that is constantally polling,
+	# should match asynch ID's to values and call their function
+	def startLongPolling(self, noWait=False):
+		# check Asynch ID's against insternal database of ID's
+		# Call return function with the value given, maybe decode from base64?
+		wait = ''
+		if(noWait == True):
+			wait = "?noWait=true"
+		# check that there isn't another thread already running, only one longPolling instance per is acceptable
+		if(self.longPollThread.isAlive()):
+			print "LongPolling is already active."
+		else:
+			# start infinite longpolling thread
+			self.longPollThread.start()
+			print "Spun off LongPolling thread"
+		return self.longPollThread # return thread instance so user can manually intervene if necessary
+
+	# Thread to constantly long poll connector and process the feedback.
+	# TODO: pass wait / noWait on to long polling thread, currently the user can set it but it doesnt actually affect anything.
+	def longPoll(self,wait = ""):
+		while True:
+			try:
+				data = r.get(self.address+'/notification/pull'+wait,headers={"Authorization":"Bearer "+self.bearer})
+				# process callbacks
+				if data.status_code != 204: # 204 means no content, do nothing
+					if 'async-responses' in json.loads(data.content).keys():
+						self.async_responses_callback(data)
+					if 'notifications' in json.loads(data.content).keys():
+						self.notifications_callback(data)
+					if 'registrations' in json.loads(data.content).keys():
+						self.registrations_callback(data)
+					if 'reg-updates' in json.loads(data.content).keys():
+						self.reg_updates_callback(data)
+					if 'de-registrations' in json.loads(data.content).keys():
+						self.de_registrations_callback(data)
+					if 'registrations-expired' in json.loads(data.content).keys():
+						self.registrations_expired_callback(data)
+					#print("data = "+data.content)
+			except:
+				# TODO: debug print statements here
+				print "\r\n[Error longPoll] failed to parse returned goodness"
+				ex_type, ex, tb = sys.exc_info()
+				traceback.print_tb(tb)
+				print sys.exc_info()
+				del tb
+
+	# internal async-requests handler.
+	def _asyncHandler(self,data):
+		print ("[DBG _asyncHandler] called, data =  %s" %str(data.content))
+		try:
+			payload = json.loads(data.content)
+			responses = payload['async-responses']
+			for entry in responses:
+				if entry['id'] in self.database['async-responses'].keys():
+					result = self.database['async-responses'].pop(entry['id']) # get the asynch object out of database
+					# fill in async-result object
+					if 'error' in entry.keys():
+						# error happened, handle it
+						result.error = connectorError('async-responses-handler',entry['status'])
+						result.error.error = entry['error']
+						result.is_done = True
+						result.callback(result)
+					else:
+						# everything is good, fill it out
+						result.result = b64decode(entry['payload'])
+						result.raw_data = payload
+						result.status = data.status_code
+						result.error = False
+						for thing in entry.keys():
+							result.extra[thing]=entry[thing]
+						result.is_done = True
+						# call associated callback function
+						if result.callback:
+							result.callback(result)
+						else:
+							print "[WARN: _asyncHandler] No callback function given"
+				#else:
+					# TODO : object not found int asynch database
+		except:
+			# TODO error handling here
+			print "\r\n[Error _asyncHandler] bad data encountered and failed to elegantly handle it. "
+			ex_type, ex, tb = sys.exc_info()
+			traceback.print_tb(tb)
+			print sys.exc_info()
+			del tb
+			return
+
+
+	# default handler for notifications. User should impliment all of these in
+	# a L2 implimentation
+	def _defaultHandler(self,data):
+		if 'async-responses' in json.loads(data.content).keys():
+			print "\r\n[Default Handler] async-responses detected : "
+			print json.loads(data.content)["async-responses"]
+		if 'notifications' in json.loads(data.content).keys():
+			print "\r\n[Default Handler] notifications' detected : "
+			print json.loads(data.content)["notifications"]
+		if 'registrations' in json.loads(data.content).keys():
+			print "\r\n[Default Handler] registrations' detected : "
+			print json.loads(data.content)["registrations"]
+		#if 'reg-updates' in json.loads(data.content).keys():
+			# removed because this happens every 10s or so, spamming the output
+			#print "\r\n[Default Handler] reg-updates detected : "
+			#print json.loads(data.content)["reg-updates"]
+		if 'de-registrations' in json.loads(data.content).keys():
+			print "\r\n[Default Handler] de-registrations detected : "
+			print json.loads(data.content)["de-registrations"]
+		if 'registrations-expired' in json.loads(data.content).keys():
+			print "\r\n[Default Handler] registrations-expired detected : "
+			print json.loads(data.content)["registrations-expired"]
+
+	# make the requests.
+	# url is the API url to hit
+	# query are the optional get params
+	# versioned tells the API whether to hit the /v#/ version. set to false for
+	#  commands that break with this, like the API and Connector version calls
+	# TODO: spin this off to be non-blocking
+	def _getURL(self, url,query={},versioned=True):
+		if versioned:
+			return r.get(self.address+self.apiVersion+url,headers={"Authorization":"Bearer "+self.bearer},params=query)
+		else:
+			return r.get(self.address+url,headers={"Authorization":"Bearer "+self.bearer},params=query)
+
+	# put data to URL with json payload in dataIn
+	def _putURL(self, url,json={},versioned=True):
+		if versioned:
+			return r.put(self.address+self.apiVersion+url,data=json,headers={"Authorization":"Bearer "+self.bearer})
+		else:
+			return r.put(self.address+url,data=json,headers={"Authorization":"Bearer "+self.bearer})
+
+
+	# extend dictionary class so we can instantiate multiple levels at once
+	class vividict(dict):
+		def __missing__(self, key):
+			value = self[key] = type(self)()
+			return value
+
+	# Initialization function, set the token used by this object.
+	def __init__(	self,
+					token,
+					webAddress="https://api.connector.mbed.com",
+					port="80",):
+		# set token
+		self.bearer = token
+		# set version of REST API
+		self.apiVersion = "/v2"
+		# Init database, used for callback fn's for various tasks (asynch, subscriptions...etc)
+		self.database = self.vividict()
+		self.database['notifications']
+		self.database['registrations']
+		self.database['reg-updates']
+		self.database['de-registrations']
+		self.database['registrations-expired']
+		self.database['async-responses']
+		#create thread for long polling
+		self.longPollThread = threading.Thread(target=self.longPoll,name="mbed-connector-longpoll")
+		self.longPollThread.daemon = True # Do this so the thread exits when the overall process does
+		# set default webAddress  and port to mbed connector
+		self.address = webAddress
+		self.port = port
+		# Initialize the callbacks
+		self.async_responses_callback = self._asyncHandler
+		self.registrations_expired_callback = self._defaultHandler
+		self.de_registrations_callback = self._defaultHandler
+		self.reg_updates_callback = self._defaultHandler
+		self.registrations_callback = self._defaultHandler
+		self.notifications_callback = self._defaultHandler
---
+++ connector/connectorError.py
@@ -0,0 +1,110 @@
+# Copyright 2014-2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+class response_codes:
+	"""Error class for connector L1 library. Contains the error type, and error string."""
+	# list of all possible error tuples
+	__errList = {
+			# GET Errors
+				# GET /
+				"get_mdc_version200":"Successful response containing version of mbed Device Connector and recent REST API version it supports.",
+
+				# GET /rest-versions
+				"get_rest_version200":"Successful response with a list of version(s) supported by the server.",
+
+				# GET /endpoints
+				"get_endpoints200":"Successful response with a list of endpoints.",
+
+				# GET /endpoint/{endpoint-name}
+				"get_resources200":"Successful response with a list of metainformation.",
+				"get_resources404":"Endpoint not found.",
+
+			# Resource Errors
+				#GET, PUT, POST, DELETE /endpoints/{endpoint-name}/{resource-path}
+				"resource200":"Successful GET, PUT, DELETE operation.",
+				"resource201":"Successful POST operation.",
+				"resource202":"Accepted. Asynchronous response ID.",
+				"resource204":"Non confirmable request made, this may or may not reach the endpoint. No Content given as response.",
+				"resource205":"No cache available for resource.",
+				"resource404":"Requested endpoint's resource is not found.",
+				"resource409":"Conflict. Endpoint is in queue mode and synchronous request can not be made. If noResp=true, the request is not supported.",
+				"resource410":"Gone. Endpoint not found.",
+				"resource412":"Request payload has been incomplete.",
+				"resource413":"Precondition failed.",
+				"resource415":"Media type is not supported by the endpoint.",
+				"resource429":"Cannot make a request at the moment, already ongoing other request for this endpoint or queue is full (for endpoints in queue mode).",
+				"resource502":"TCP or TLS connection to endpoint is not established.",
+				"resource503":"Operation cannot be executed because endpoint is currently unavailable.",
+				"resource504":"Operation cannot be executed due to a time-out from the endpoint.",
+
+			# Subscription / Notification Errors
+				# PUT /subscriptions/{endpoint-name}/{resource-path}
+				"subscribe200":"Successfully subscribed.",
+				"subscribe202":"Accepted. Asynchronous response ID.",
+				"subscribe404":"Endpoint or its resource not found.",
+				"subscribe412":"Cannot make a subscription for a non-observable resource.",
+				"subscribe413":"Cannot make a subscription due to failed precondition.",
+				"subscribe415":"Media type is not supported by the endpoint.",
+				"subscribe429":"Cannot make subscription request at the moment due to already ongoing other request for this endpoint or (for endpoints in queue mode) queue is full or queue was cleared because endpoint made full registration.",
+				"subscribe502":"Subscription failed.",
+				"subscribe503":"Subscription could not be established because endpoint is currently unavailable.",
+				"subscribe504":"Subscription could not be established due to a time-out from the endpoint.",
+
+				# DELETE /subscriptions/{endpoint-name}/{resource-path}
+				# DELETE /subscriptions
+				"unsubscribe204":"Successfully removed subscription.",
+				"unsubscribe404":"Endpoint or endpoint's resource not found.",
+
+				# GET /subscriptions/{endpoint-name}/{resource-path}
+				"get_resource_subscription200":"Resource is subscribed.",
+				"get_resource_subscription404":"Resource is not subscribed.",
+
+				# GET /subscriptions/{endpoint-name}
+				"get_endpoint_subscription200":"List of subscribed resources.",
+				"get_endpoint_subscription404":"Endpoint not found or there are no subscriptions for that endpoint.",
+
+				# DELETE /subscriptions/{endpoint-name}
+				"delete_endpoint_subscription204":"Successfully removed.",
+				"delete_endpoint_subscription404":"Endpoint not found.",
+
+				# GET /subscriptions - Presubscription Data
+				# Nothing yet?
+
+			# Callback
+				# PUT /notification/callback
+				"put_callback_url204":"Successfully subscribed.",
+				"put_callback_url400":"Given URL is not accessible.",
+
+				# GET /notification/callback
+				"get_callback_url200":"URL found.",
+				"get_callback_url404":"Callback URL does not exist.",
+
+				# DELETE /notification/callback
+				"delete_callback_url204":"Successfully removed.",
+				"delete_callback_url404":"Callback URL does not exist.",
+
+			# Long polling
+				# GET /notification/pull
+				"longpoll200":"OK.",
+				"longpoll204":"No new notifications.",
+
+			# Limits
+				# GET /limits
+				"limit200":"OK.",
+	}
+
+	# set the error type by querying the __errList
+	def _setError(self,errType):
+		if errType in self.__errList.keys():
+			return self.__errList[errType]
+		else:
+			return "ERROR: Unknown error."
+
+	def __init__(self,errParent,status_code):
+		self.status_code = status_code
+		self.errType = str(errParent)+str(status_code)
+		self.error = self._setError(self.errType)
+
+
