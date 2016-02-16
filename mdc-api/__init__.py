97e26241de7c27dc73a7d869b2d762c094667f9b
Austin <austin.blackstone@arm.com>
2016-02-16 00:56:42 +0000

    added packaging info, setup.py and init.py

---
+++ mdc-api/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2014-2015 ARM Limited
+#
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details.
+
+from connectorError import response_codes
+from mdc_api import asyncResult
+from mdc_api import connector
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='mdc_api',
+      version='1.0.0',
+      description='REST interface library for mbed Device Connector (connector.mbed.com)',
+      long_description='A the longer, more marketing appropriate description',
+      url='http://github.com/armmbed/mdc-api-python',
+      author='ARM mbed',
+      author_email='support@mbed.com',
+      license='Apache 2.0',
+      packages=['mdc_api'],
+      keywords='ARM mbed connector device mDC mDS',
+      install_requires=[
+          'requests',
+      ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      #entry_points={
+      #    'console_scripts': ['funniest-joke=funniest.command_line:main'],
+      #},
+      zip_safe=False)
