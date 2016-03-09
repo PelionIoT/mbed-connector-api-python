6a5ef14bd4b6ea730c2e0a80eb94aaaec2cb26fd
Austin <austin.blackstone@arm.com>
2016-03-09 05:48:15 +0000

    making changes to build test file

--- mbed_connector_api/tests/bin/build_linux_client.sh
+++ mbed_connector_api/tests/bin/build_linux_client.sh
@@ -1,10 +1,10 @@
 export MBED_USER_ID=mdc-api-python
 cd mbed_connector_api/tests/bin
-git clone https://github.com/bridadan/mbed-client-linux-example.git
-rm mbed-client-linux-example/source/security.h
-cp ./security.h mbed-client-linux-example/source/security.h
+git clone https://github.com/BlackstoneEngineering/mbed-client-linux-ci.git
+rm mbed-client-linux-ci/source/security.h
+cp ./security.h mbed-client-linux-ci/source/security.h
 ls
-cd mbed-client-linux-example
+cd mbed-client-linux-ci
 echo "{\"endpoint-lifetime\": 60}" > config.json
 yotta target x86-linux-native
 yotta build
